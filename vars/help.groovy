f07bfcac0c5740bd0f0571d97ffa050f3663d51a
Julio Chaves <chaves.julio@gmail.com>
2020-12-11 22:12:04 +0000

    fix: adjusting shares library

--- vars/help.groovy
+++ vars/help.groovy
@@ -1,10 +1,6 @@
 import me.juliochaves.Help
 
 def call(Map config) {
-  node {
-    stage('Build') {
-      def help = new Help(this)
-      help.show()
-    }
-  }
+  def help = new Help(this)
+  help.show()
 }
